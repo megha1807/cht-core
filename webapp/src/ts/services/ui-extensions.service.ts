901f8ba45b7c046b47c2e401e35f7991c122bf03
megha1807 <meghakumarig5@gmail.com>
2026-03-27 07:37:58 +0530

    fix(#10670): correct HtmlElement to HTMLElement

--- webapp/src/ts/services/ui-extensions.service.ts
+++ webapp/src/ts/services/ui-extensions.service.ts
@@ -15,7 +15,7 @@ interface UiExtensionProperties {
 
 interface UiExtension {
   readonly properties: UiExtensionProperties;
-  readonly Element: HtmlElement;
+  readonly Element: HTMLElement;
 }
 
 @Injectable({

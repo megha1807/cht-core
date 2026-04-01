ee5074a84c6590ae648b07b2b69d28c4cdf7ac08
megha1807 <meghakumarig5@gmail.com>
2026-04-01 11:51:21 +0530

    fix(#8867): update getMessageActions permission checks in tests

--- webapp/tests/karma/ts/services/fast-action-button.service.spec.ts
+++ webapp/tests/karma/ts/services/fast-action-button.service.spec.ts
@@ -166,7 +166,7 @@ describe('Fast Action Button service', () => {
       const actions = await service.getMessageActions(context);
 
       expect(actions.length).to.equal(1);
-      expect(authService.has.args[0][0]).to.have.members([ 'can_view_message_action', 'can_edit' ]);
+      expect(authService.has.args[0][0]).to.have.members([ 'can_view_message_action', 'can_edit', 'can_update_contacts' ]);
       assertSendMessageAction(actions[0], context);
 
       sinon.resetHistory();
@@ -175,7 +175,7 @@ describe('Fast Action Button service', () => {
       const mobileActions = await service.getMessageActions(context);
 
       expect(mobileActions.length).to.equal(1);
-      expect(authService.has.args[0][0]).to.have.members([ 'can_view_message_action', 'can_edit' ]);
+      expect(authService.has.args[0][0]).to.have.members([ 'can_view_message_action', 'can_edit', 'can_update_contacts' ]);
       // Message tab doesn't use mailto in mobile display, the action should be the same as in desktop
       assertSendMessageAction(actions[0], context);
     });
@@ -190,7 +190,7 @@ describe('Fast Action Button service', () => {
       const actions = await service.getMessageActions(context);
 
       expect(actions.length).to.equal(1);
-      expect(authService.has.args[0][0]).to.have.members([ 'can_view_message_action', 'can_edit' ]);
+      expect(authService.has.args[0][0]).to.have.members([ 'can_view_message_action', 'can_edit', 'can_update_contacts' ]);
       assertSendMessageAction(actions[0], context);
     });
   });
@@ -222,7 +222,7 @@ describe('Fast Action Button service', () => {
       expect(actions.length).to.equal(7);
       expect(authService.has.args).to.have.deep.members([
         [ 'can_view_call_action' ],
-        [ [ 'can_view_message_action', 'can_edit' ] ],
+        [ [ 'can_view_message_action', 'can_edit', 'can_update_contacts' ] ],
         [ [ 'can_edit', 'can_update_contacts', 'can_create_places' ] ],
         [ [ 'can_edit', 'can_update_contacts', 'can_create_places' ] ],
         [ [ 'can_edit', 'can_update_contacts', 'can_create_people' ] ],
